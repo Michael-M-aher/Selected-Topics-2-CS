ca771a625e2a58ef6207bcab3614e6d3c3f07024
Michael Maher <michael.maher.nagi@gmail.com>
2024-05-17 02:13:37 +0300

    add problems

---
+++ Assignment 4/Q - Dark roads.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define EPS 1e-9
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define mem(arr, x) memset(arr,x,sizeof arr)
+#define endl '\n'
+#define PI acos(-1)
+#define Ones(n) __builtin_popcount(n)
+#define Onesl(n) __builtin_popcountll(n)
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define precision(x) fixed << setprecision(x)
+#define sz(x) (int)x.size()
+#define pb push_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rep2(i, a, b) for (int i = a; i <= b; ++i)
+#define repr(i, a, b) for (int i = a; i > b; --i)
+#define repr2(i, a, b) for (int i = a; i >= b; --i)
+#define repit(it, x) for (auto it = x.begin(); it != x.end(); ++it)
+#define reprit(it, x) for (auto it = x.rbegin(); it != x.rend(); ++it)
+#define read(x, n) for (int i = 0; i < n; ++i) cin >> x[i]
+#define read2(x, n, m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> x[i][j]
+#define print(x) for (int i = 0; i < sz(x); ++i) cout << x[i] << " \n"[i == sz(x) - 1]
+#define print2(x) for (int i = 0; i < sz(x); ++i) {for (int j = 0; j < sz(x[i]); ++j) cout << x[i][j] << " \n"[j == sz(x[i]) - 1];}
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+const ll MOD = 1e9 + 7;
+const ll N = 2e5 + 5;
+const int OO = 0x3f3f3f3f;
+const ll LOO = 0x3f3f3f3f3f3f3f3f;
+const int NINF = -1e9;
+const int INF = 1e9;
+int dx4[] = {0, 0, -1, 1};
+int dy4[] = {-1, 1, 0, 0};
+int dx8[] = {0, 0, -1, -1, -1, 1, 1, 1};
+int dy8[] = {-1, 1, -1, 0, 1, -1, 0, 1};
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int n,m;
+
+vi parent;
+vi minWeight;
+vector<bool> inMST;
+vector<vector<pii>> adj;
+
+int getMinV(){
+    int minW = INT_MAX, minWVertex;
+    for (int v = 0; v < n; v++){
+        if (!inMST[v] && minWeight[v] < minW){
+            minW = minWeight[v], minWVertex = v;
+        }
+    }
+    return minWVertex;
+}
+
+void MST(){
+    parent.assign(n, -1);
+    minWeight.assign(n, INT_MAX);
+    inMST.assign(n, false);
+
+    minWeight[0] = 0;
+
+    parent[0] = -1;
+
+    for (int count = 0; count < n - 1; count++) {
+        int u = getMinV();
+        inMST[u] = true;
+        for (auto [v,w] : adj[u]) {
+            if (!inMST[v] && w < minWeight[v]) {
+                parent[v] = u, minWeight[v] = w;
+            }
+        }
+    }
+}
+
+
+void solve() {
+    while (cin >> n >> m && (n || m)) {
+        adj.clear();
+        adj.assign(n, vector<pii>());
+        ll total = 0;
+        rep(i, 0, m) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            adj[u].emplace_back(v, w);
+            adj[v].emplace_back(u, w);
+            total += w;
+        }
+        MST();
+        ll ans = 0;
+        rep(i, 0, n) {
+            if (parent[i] != -1) {
+                ans += minWeight[i];
+            }
+        }
+        cout << total - ans << endl;
+
+    }
+}
+
+
+int main() {
+    fast();
+//    freopen("input.txt","rt",stdin);
+//    freopen("output.txt","wt",stdout);
+    int t = 1;
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
